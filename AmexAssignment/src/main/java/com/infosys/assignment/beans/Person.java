f34fcba2595631e3ef84fd44e8d7c0ef73412254
david <dvmill1994@gmail.com>
2018-08-27 01:20:42 -0400

    addded id to person class

--- AmexAssignment/src/main/java/com/infosys/assignment/beans/Person.java
+++ AmexAssignment/src/main/java/com/infosys/assignment/beans/Person.java
@@ -1,6 +1,12 @@
 package com.infosys.assignment.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 public class Person {
+	private int id;
 	private String name;
 	private String phonenumber;
 	private String address;
@@ -10,36 +16,18 @@ public class Person {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + id;
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((phonenumber == null) ? 0 : phonenumber.hashCode());
 		return result;
 	}
 	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		if (address == null) {
-			if (other.address != null)
-				return false;
-		} else if (!address.equals(other.address))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (phonenumber == null) {
-			if (other.phonenumber != null)
-				return false;
-		} else if (!phonenumber.equals(other.phonenumber))
-			return false;
-		return true;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 	public String getName() {
 		return name;
@@ -58,10 +46,44 @@ public class Person {
 	}
 	public void setAddress(String address) {
 		this.address = address;
-	}	
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (phonenumber == null) {
+			if (other.phonenumber != null)
+				return false;
+		} else if (!phonenumber.equals(other.phonenumber))
+			return false;
+		return true;
+	}
+
 
 	@Override
 	public String toString() {
-		return "Person [name=" + name + ", phonenumber=" + phonenumber + ", address=" + address + "]";
-	}
+		return "Person [id=" + id + ", name=" + name + ", phonenumber=" + phonenumber + ", address=" + address + "]";
+	}	
+	
+	
+
 }
