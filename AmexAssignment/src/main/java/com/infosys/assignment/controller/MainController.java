1c35452a133455019143113aa4342c25c563fa8e
david <dvmill1994@gmail.com>
2018-08-27 01:20:00 -0400

    Data

--- AmexAssignment/src/main/java/com/infosys/assignment/controller/MainController.java
+++ AmexAssignment/src/main/java/com/infosys/assignment/controller/MainController.java
@@ -1,11 +1,8 @@
 package com.infosys.assignment.controller;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.infosys.assignment.beans.Person;
@@ -16,16 +13,8 @@ public class MainController {
 
     @Autowired
     private PersonServiceimpl psi;
-
-  /*  @RequestMapping(value = "/person", method = RequestMethod.GET)
-    public List<Person> listPeople() {
-        
-        return psi.listPeople();
-    }*/
-    @RequestMapping(value = "/person/{name}", method = RequestMethod.GET)
-    public Person findByName(@PathVariable String name) {
-    	Person r=psi.findByName(name);
-    	System.out.println(r.toString());
-        return r;
+    @RequestMapping(value = "/person/{id}", method = RequestMethod.GET)
+    public Person findByName(@PathVariable int id) {
+        return psi.findById(id);
     }
 }
